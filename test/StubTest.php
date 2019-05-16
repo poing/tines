726ebdbf12caa28ebb0c5d8beec2b4dcd284655e
brian.lavallee@invite-comm.jp <Brian LaVallee>
2019-05-16 07:34:08 +0000

    fix phpunit

--- test/StubTest.php
+++ test/StubTest.php
@@ -2,6 +2,7 @@
 namespace Tines\Test;
 
 use Poing\Tines\Stub;
+use PHPUnit\Framework\TestCase;
 
 /**
  * @coversDefaultClass Poing\Ylem\Stub
@@ -13,7 +14,6 @@ class StubTest extends TestCase
    {
        $this->assertTrue(true);
    }
-}
 
   /**
    * @covers Poing\Tines\Stub::probe()
